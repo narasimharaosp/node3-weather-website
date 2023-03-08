fcb0006083bd28449ffaa50ca5651516f0c07c43
sp_nrao <simha.sp@gmail.com>
2023-03-08 23:12:10 +0530

    port being read from env now

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3002
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -94,6 +95,6 @@ app.get('*', (req, res) => {
   })
 })
 
-app.listen(3002, () => {
+app.listen(port, () => {
   console.log('http://localhost:3002')
 })
