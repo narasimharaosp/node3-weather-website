0d00a64cbb1b27055c6753837c10144368e1c969
sp_nrao <simha.sp@gmail.com>
2023-03-08 23:28:59 +0530

    day or night flag added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,8 +11,10 @@ const forecast = (lat, long, callback) => {
     } else if (body.error) {
       callback('Unable to find location', undefined)
     } else {
-      const { temperature, feelslike, weather_descriptions } = body.current
-      const msg = weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out. It feels like ' + feelslike + ' degrees out.'
+      const { temperature, feelslike, weather_descriptions, is_day } = body.current
+      let msg = weather_descriptions[0] + '. It is currently ' + temperature + ' degrees out. It feels like ' + feelslike + ' degrees out.'
+      const dayString = is_day === 'no' ? 'night' : 'day'
+      msg += ` It's ${dayString} here...!!!`
       callback(undefined, msg)
     }
   })
